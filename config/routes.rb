f35c1db2d9c08e61ead8ad1a9a9573bcd58b0ae8
Ezequiel <exe.zaid@gmail.com>
2010-10-13 20:44:16 -0300

    vistas y timeline

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 CursoTwitter::Application.routes.draw do
+  resources :tweets
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,7 +49,7 @@ CursoTwitter::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+  root :to => "tweets#timeline"
 
   # See how all your routes lay out with "rake routes"
 
